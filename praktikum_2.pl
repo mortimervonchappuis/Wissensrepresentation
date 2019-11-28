7cf9617ef91701e4a39305473531457f2b913aa2
mortimer <mortimervonchappuis@protonmail.com>
2019-11-28 12:08:24 +0100

    fixed reachable path finding

--- praktikum_2.pl
+++ praktikum_2.pl
@@ -5,11 +5,15 @@ maze_1() :- load_layout(maze_1).
 
 empty(C) :- not(at(C, _)).
 
+trespass(C) :- (at(C, E), not(E = wall); empty(C)).
+
 visible(C, C, _) :- not(at(C, wall)).
 visible(C1, C3, D) :- adjacent(C2, D, C3), not(at(C2, wall)), visible(C1, C2, D).
 
-reachable(C, C, []) :- not(at(C, wall)).
-reachable(C1, C3, [D|Ds]) :- adjacent(C1, D, C2), not(at(C1, wall)), sound_path(Ds), reachable(C2, C3, Ds).
+reachable(C1, C2, Ds) :- reach(C1, C2, Ds, []).
+
+reach(C1, C2, [D], _) :- adjacent(C1, D, C2), trespass(C2).
+reach(C1, C3, [D|Ds], Ps) :- append(Ps, [C1], Ps_), adjacent(C1, D, C2), not(member(C2, Ps_)), trespass(C2), reach(C2, C3, Ds, Ps_).
 
 goalachivement() :- not(at(_, ghost)), at(_, capsule),  at(_, food),  at(_, pacman).
 
@@ -20,25 +24,3 @@ goalachivement() :- not(at(_, ghost)), at(_, capsule),  at(_, food),  at(_, pacm
 % adjacent(cell(1, 9), X, cell(1, 10)).
 % reachable(cell(1, 9), cell(1, 10), X).
 % reachable(cell(1, 1), cell(5, 18), X).
-
-sound_path([]).
-sound_path([north]).
-sound_path([east]).
-sound_path([south]).
-sound_path([west]).
-sound_path([D1, D2|Ds]) :- redundant(D1, D2), sound_path([D2|Ds]).
-
-redundant(D1, D2) :- not((D1 = south, D2 = north)), not((D2 = south, D1 = north)), not((D1 = east, D2 = west)), not((D2 = east, D1 = west)).
-
-% path(C, C, _) :- (not(at(C, wall))).
-% path(C1, C2, [C1, C2]) :- adjacent(C1, _, C2).
-% path(C1, C3, Cs) :- append(Cs,[C1], C), adjacent(C1, _, C2), not(member(C2, C)), path(C2, C3, C).
-
-reach_proxy(C1,C2,D) :- reach(C1,C2,D,[]).
-reach(C1,C2,[D], _) :- adjacent(C1,D,C2), (at(C2, E), E \= wall; empty(C2)).
-reach(C1,C2,[D|Ds],P) :- append(P,[C1],Ps), adjacent(C1,D,CM), not(member(CM,Ps)), (at(CM, E), E \= wall; empty(CM)), reach(CM,C2,Ds,Ps).
-
-reach_proxy(C1,C2) :- reach(C1,C2,[]).
-reach(C1,C2, _) :- adjacent(C1,D,C2), (at(C2, E), E \= wall; empty(C2)).
-reach(C1,C2,P) :- append(P,[C1],Ps), adjacent(C1,_,CM), not(member(CM,Ps)), (at(CM, E), E \= wall; empty(CM)), reach(CM,C2,Ps).reach_proxy(C1,C2,D) :- reach(C1,C2,D,[]).
-
