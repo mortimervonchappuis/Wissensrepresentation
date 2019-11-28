5ab096cc7aad90aa2e31111fc3677cec526ca171
mortimer <mortimervonchappuis@protonmail.com>
2019-11-28 17:57:38 +0100

    seperated goals

--- praktikum_2.pl
+++ praktikum_2.pl
@@ -15,7 +15,21 @@ reachable(C1, C2, Ds) :- reach(C1, C2, Ds, []).
 reach(C1, C2, [D], _) :- adjacent(C1, D, C2), trespass(C2).
 reach(C1, C3, [D|Ds], Ps) :- append(Ps, [C1], Ps_), adjacent(C1, D, C2), not(member(C2, Ps_)), trespass(C2), reach(C2, C3, Ds, Ps_).
 
-goalachivement() :- not(at(_, ghost)), at(_, capsule),  at(_, food),  at(_, pacman).
+goalachivement(A, B, C, D) :- ghost(A), pacman(B) , capsule(C), food(D).
+
+ghost(ghost) :- at(_, ghost).
+ghost(no_ghost) :- not(at(_, ghost)).
+
+pacman(pacman) :- at(_, pacman).
+pacman(no_pacman) :- not(at(_, pacman)).
+
+capsule(capsule) :- at(_, capsule).
+capsule(no_capsule) :- not(at(_, capsule)).
+
+food(food) :- at(_, food).
+food(no_food) :- not(at(_, food)).
+
+
 
 % empty(cell(2, 9)).
 % empty(cell(2, 8)).
@@ -24,3 +38,4 @@ goalachivement() :- not(at(_, ghost)), at(_, capsule),  at(_, food),  at(_, pacm
 % adjacent(cell(1, 9), X, cell(1, 10)).
 % reachable(cell(1, 9), cell(1, 10), X).
 % reachable(cell(1, 1), cell(5, 18), X).
+% goalachivement(A, B, C, D).
